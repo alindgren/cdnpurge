d223109fcc718a624e014849c4abe1fa7874e764
Alex Lindgren <alindgren@flightpath.com>
2017-09-26 13:40:16 -0400

    update controller

--- src/Web/Controllers/API/PurgeCdnAPIController.cs
+++ src/Web/Controllers/API/PurgeCdnAPIController.cs
@@ -10,14 +10,22 @@ namespace PurgeCDN.Web.Controllers.API
     public class PurgeCdnAPIController : UmbracoAuthorizedJsonController
     {
         [HttpGet]
-        public DialogViewModel InitDialog()
+        public DialogViewModel InitDialog(int nodeId)
         {
+            var node = Umbraco.TypedContent(nodeId);
+            if (node == null)
+            {
+                return new DialogViewModel() { Status = "error", ErrorMessage = "Invalid node id: " + nodeId };
+            }
+
             // if KeyCDN Client ID and/or Secret are missing, return error message
             if (!CdnPurger.IsActive())
                 return new DialogViewModel() { Status = "error", ErrorMessage = "KeyCDN Client ID and/or Secret not set." };
 
             return new DialogViewModel()
             {
+                NodeId = nodeId,
+                NodeName = node.Name,
                 Status = "ok"
             };
         }
--- src/Web/Models/DialogViewModel.cs
+++ src/Web/Models/DialogViewModel.cs
@@ -2,6 +2,8 @@
 {
     public class DialogViewModel
     {
+        public int NodeId { get; set; }
+        public string NodeName { get; set; }
         public string ErrorMessage { get; set; }
         public string Status { get; set; } // ok, error
     }
